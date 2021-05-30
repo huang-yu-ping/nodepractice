0e00eae6316a8e21bef49ca62328f7dd67615d9d
yuping <xx4685441@gmail.com>
2021-05-30 13:21:35 +0800

    update

--- crawler/app-promise.js
+++ crawler/app-promise.js
@@ -2,6 +2,19 @@ const axios = require('axios');
 const fs = require('fs');
 
 
+const now = new Date();
+let year = now.getFullYear();
+let month = now.getMonth() + 1;
+let date = now.getDate();
+
+month = month < 10 ?  "0" + month : month;
+date = date < 10 ? "0" + date : date;
+
+let today = year + month + date;
+
+console.log(today);
+
+
 function readStock() {
   return new Promise(function(res, rej) {
     fs.readFile('stock.txt', function(err, data) {
@@ -16,7 +29,13 @@ function readStock() {
 
 readStock()
   .then((data) => {
-    return axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210528&stockNo=${data}&_=1622187642107`)
+    return axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+      params: {
+        response: "json",
+        date: today,
+        stockNo: data,
+      }
+    })
     .then(function(res) {
       console.log(res);
       if (res.data.stat === 'OK') {
