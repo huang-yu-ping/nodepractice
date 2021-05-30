56d7638ab952d71db853c825ebba9687e7926b64
yuping <xx4685441@gmail.com>
2021-05-30 12:13:08 +0800

    style

--- crawler/app-promise.js
+++ crawler/app-promise.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 
 
 function readStock() {
-  return new Promise(function(res, rej) {
-    fs.readFile('stock.txt', function(err, data) {
-      if(err) {
+  return new Promise(function (res, rej) {
+    fs.readFile('stock.txt', function (err, data) {
+      if (err) {
         rej(err);
       } else {
         res(data.toString());
@@ -16,13 +16,13 @@ function readStock() {
 
 readStock()
   .then((data) => {
-    return axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210528&stockNo=${data}&_=1622187642107`).then(function(res) {
+    return axios.get(`https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=20210528&stockNo=${data}&_=1622187642107`).then(function (res) {
       console.log(res);
-      if(res.data.stat === 'OK') {
+      if (res.data.stat === 'OK') {
           console.log(res.data.date);
           console.log(res.data.title);
       }
-   })
+    })
   })
   
     
