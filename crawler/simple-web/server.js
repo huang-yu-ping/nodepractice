cf4d0d56f0e64418bfacf1d0a416788e9541c2e6
yuping <xx4685441@gmail.com>
2021-06-06 15:33:09 +0800

    add new file

---
+++ crawler/simple-web/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain;charset=UTF-8");
+
+  switch (req.url) {
+      case '/':
+          res.end('hi, index page');
+      case 'test':
+          res.end('test page');
+          break;
+      default:
+          res.writeHead(404);
+          res.end('404 not found');
+      
+  }
+});
+
+server.listen(3000, () => console.log('server already running...'));
+
+
+
