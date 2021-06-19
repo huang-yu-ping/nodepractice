e3a8e39648e32ad48b26b2ddd3d0684d75dd65e0
yuping <xx4685441@gmail.com>
2021-06-19 17:17:30 +0800

    like vue

--- simple-express/myweb/public/javascripts/api.js
+++ simple-express/myweb/public/javascripts/api.js
@@ -10,11 +10,23 @@
 }
 getApi()*/
 
+//const { compile } = require("pug");
+
 //axios
 async function getApi() {
+    let templatehtml = $('#mytemplate').html();
+    let compiler = _.template(templatehtml);
     let res = await axios.get('api/stocks');
-    console.log('123')
-    console.log(res);
+    console.log(res.data);
+    _.each(res.data, function(data) {
+       // console.log(data);
+        let str = compiler({
+            "stock_id": data.stock_id,
+            "stock_name": data.stock_name
+        })
+        console.log(str);
+        $(str).appendTo('#app');
+    })
 }
 getApi();
 
